65767927bb51d8defb595254a8164b58414a37ef
Myslitel666 <tretyakov_02@bk.ru>
2025-09-10 20:27:52 +0500

    i

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,78 +1,49 @@
 <script lang="ts">
   import { themeStore } from "svelte-elegant/stores";
   import { onMount, onDestroy } from "svelte";
-  import { ButtonBox, Checkbox, TextField } from "svelte-elegant";
-  import { Play, Pause } from "svelte-elegant/icons-elegant";
+  import { Button, Checkbox, TextField } from "svelte-elegant";
 
   let isInitialized = false;
-  let isStopped = true;
-  let rememberedTime = 0;
-  let elapsedTime = 0;
-  const timeStartConst = "00";
-  let minutes = timeStartConst;
-  let seconds = timeStartConst;
-  let ms = timeStartConst;
-  let timerInterval: number | null = null;
-
-  function formatTimeUnits(value: number) {
-    return (value % 60).toString().padStart(2, "0");
-  }
-
-  function initialTimer() {
-    const startTime = Date.now();
-
-    timerInterval = setInterval(() => {
-      elapsedTime = rememberedTime + Date.now() - startTime;
-
-      minutes = formatTimeUnits(Math.floor(elapsedTime / 60000));
-      seconds = formatTimeUnits(Math.floor(elapsedTime / 1000));
-      ms = formatTimeUnits(Math.floor(elapsedTime / 16.666));
-    }, 60); // Интервал не важен, можно 16ms (~60fps)
-  }
-
-  function clearTimer() {
-    if (timerInterval) {
-      clearInterval(timerInterval);
-      timerInterval = null;
-    }
-  }
+  let isUseNumbers = true;
+  let isUseLowercase = true;
+  let isUseUppercase = true;
+  let isUseSymbols = true;
 
   onMount(() => {
     isInitialized = true;
-  });
-
-  // ОЧИЩАЕМ ТАЙМЕР ПРИ УНИЧТОЖЕНИИ КОМПОНЕНТА
-  onDestroy(() => {
-    if (timerInterval) {
-      clearInterval(timerInterval);
-      timerInterval = null;
-    }
   });
 </script>
 
 <div class="page">
   <div class="option" style:margin-top="10px">
-    <p style:margin-right="4px">Enter the password length:</p>
-    <TextField type="number" label="Length" width="145px" textAlign="right" />
+    <p style:margin-right="7px">Enter the password length:</p>
+    <TextField
+      type="number"
+      label="Length"
+      width="145px"
+      value="10"
+      textAlign="right"
+    />
   </div>
   <div>
     <div class="option">
-      <Checkbox />
+      <Checkbox bind:isActive={isUseNumbers} />
       <p style:margin-left="4px">Use Numbers</p>
     </div>
     <div class="option">
-      <Checkbox />
+      <Checkbox bind:isActive={isUseLowercase} />
       <p style:margin-left="4px">Use Lowercase (a-z)</p>
     </div>
     <div class="option">
-      <Checkbox />
+      <Checkbox bind:isActive={isUseUppercase} />
       <p style:margin-left="4px">Use Uppercase (A-Z)</p>
     </div>
     <div class="option">
-      <Checkbox />
+      <Checkbox bind:isActive={isUseSymbols} />
       <p style:margin-left="4px">Use Symbols</p>
     </div>
   </div>
+  <Button marginTop="3px">Generate Password</Button>
 </div>
 
 <style>
